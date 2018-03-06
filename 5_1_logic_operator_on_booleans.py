01bddcafbbf79d3f06bd846af86381e7faaf97b0
nedaa4576 <36712195+nedaa4576@users.noreply.github.com>
2018-03-06 08:40:29 +0330

    Update 5_1_logic_operator_on_booleans.py
    vlue of both is true and in print result of three variable

--- 5_1_logic_operator_on_booleans.py
+++ 5_1_logic_operator_on_booleans.py
@@ -1,11 +1,11 @@
 a= True
-b= False
+b= True
 print("a is {0}".format(a))
 print("b is {0}".format(b))
 print("~a is {0}".format(not a))
 print("~b is {0}".format(not b))
-print("a & b is {0}".format(a and b)) # True if both of them are True
-print("a or b is {0}".format(a or b)) # True if one or both of them are True
+print("a & b is {0}".format((a and b) or a)) # (True if both of them are True) true if one of them true
+print("a or b is {0}".format((a or b) and b) # (True if one or both of them are True) true if both of them is true
 
 
 #################################################
